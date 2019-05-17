98421869892d66970cc5bef2312e4a7967cf73d6
mohe2015 <Moritz.Hedtke@t-online.de>
2019-05-17 18:56:24 +0200

    Switch to ids

--- header.php
+++ header.php
@@ -12,25 +12,14 @@ if (empty($_SESSION['token'])) {
 require_once 'config.php';
 try {
     $db = new PDO($database['url'], $database['username'], $database['password'], array(
-    //  PDO::ATTR_PERSISTENT => true,
+      PDO::ATTR_PERSISTENT => true,
       PDO::ATTR_EMULATE_PREPARES => false,
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    //  PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
     ));
 
-/*
-    $stmt = $db->prepare("CREATE DATABASE IF NOT EXISTS $database[dbname]");
-    $stmt->execute();
-    $stmt->fetchAll();
-    $stmt->closeCursor();
-
-    $stmt1 = $db->prepare("USE $database[dbname];");
-    $stmt1->execute();
-    $stmt1->fetchAll();
-    $stmt1->closeCursor();
-*/
     $stmt = $db->query("CREATE TABLE IF NOT EXISTS projects (
-    title VARCHAR(256) PRIMARY KEY NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+    title VARCHAR(256) NOT NULL,
     info VARCHAR(4096) NOT NULL,
     place VARCHAR(256) NOT NULL,
     costs DECIMAL(4,2) NOT NULL,
@@ -45,20 +34,21 @@ try {
     $stmt->closeCursor();
 
     $stmt = $db->query("CREATE TABLE IF NOT EXISTS users (
-    name VARCHAR(64) PRIMARY KEY NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(64) NOT NULL,
     password VARCHAR(255) NOT NULL,
     type ENUM('student', 'teacher', 'admin') NOT NULL,
-    project_leader VARCHAR(256),
+    project_leader INTEGER,
     class VARCHAR(8),
     grade TINYINT UNSIGNED,
     away BOOLEAN,
-    in_project VARCHAR(256),
+    in_project INTEGER,
     FOREIGN KEY (project_leader)
-      REFERENCES projects(title)
+      REFERENCES projects(id)
       ON UPDATE RESTRICT
       ON DELETE RESTRICT,
     FOREIGN KEY (in_project)
-      REFERENCES projects(title)
+      REFERENCES projects(id)
       ON UPDATE RESTRICT
       ON DELETE RESTRICT
     );");
@@ -66,23 +56,22 @@ try {
 
     $stmt = $db->query("CREATE TABLE IF NOT EXISTS choices (
     rank TINYINT NOT NULL,
-    project VARCHAR(256) NOT NULL,
-    student VARCHAR(64) NOT NULL,
+    project INTEGER NOT NULL,
+    student INTEGER NOT NULL,
     PRIMARY KEY(project,student),
     FOREIGN KEY (project)
-      REFERENCES projects(title)
+      REFERENCES projects(id)
       ON UPDATE RESTRICT
       ON DELETE RESTRICT,
     FOREIGN KEY (student)
-      REFERENCES users(name)
+      REFERENCES users(id)
       ON UPDATE RESTRICT
       ON DELETE RESTRICT
     );");
     $stmt->closeCursor();
 
-    //$password_hash = password_hash("admin", PASSWORD_DEFAULT, $options);
     //$stmt = $db->prepare('INSERT INTO users (name, password, type) VALUES (:name, :password, "admin")');
-    //$stmt->execute(array('name' => 'admin', 'password' => $password_hash));
+    //$stmt->execute(array('name' => 'admin', 'password' => password_hash("admin", PASSWORD_DEFAULT, $options)));
 } catch (PDOException $e) {
     print "Error!: " . $e . "<br/>";
     die();
