027f84315547d7796778e9d829206fd4192cd937
mohe2015 <Moritz.Hedtke@t-online.de>
2019-05-17 21:57:44 +0200

    Improve navbar

--- head.php
+++ head.php
@@ -1,5 +1,20 @@
 <?php
 require_once 'header.php';
+
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
+
+function active($path) {
+  echo startsWith($_SERVER["REQUEST_URI"], $path) ? 'class="active"' : '';
+}
+
+function active_exact($path) {
+  echo $_SERVER["REQUEST_URI"] === $path ? 'class="active"' : '';
+}
+
 ?><!doctype html>
 <html lang="de">
   <head>
@@ -12,10 +27,10 @@ require_once 'header.php';
   <body>
     <nav>
       <ul>
-         <li><a href="/"><i class="fas fa-home"></i><span class="hidden-small"> Startseite</span></a></li>
-         <li><a class="active" href="/projects"><i class="fas fa-users"></i><span class="hidden-small"> Projekte</span></a></li>
-         <li><a href="/teachers"><i class="fas fa-chalkboard-teacher"></i><span class="hidden-small"> Lehrer</span></a></li>
-         <li><a href="/students"><i class="fas fa-user"></i><span class="hidden-small"> Schüler</span></a></li>
+         <li><a <?php active_exact("/") ?> href="/"><i class="fas fa-home"></i><span class="hidden-small"> Startseite</span></a></li>
+         <li><a <?php active("/project") ?> href="/projects"><i class="fas fa-users"></i><span class="hidden-small"> Projekte</span></a></li>
+         <li><a <?php active("/teacher") ?> href="/teachers"><i class="fas fa-chalkboard-teacher"></i><span class="hidden-small"> Lehrer</span></a></li>
+         <li><a <?php active("/student") ?> href="/students"><i class="fas fa-user"></i><span class="hidden-small"> Schüler</span></a></li>
          <li class="float-right"><a href="#"><i class="fas fa-ban"></i><span class="hidden-small"> Wahl beenden</span></a></li>
          <li class="float-right"><a href="#"><i class="fas fa-sign-out-alt"></i><span class="hidden-small"> Abmelden</span></a></li>
         </li>
