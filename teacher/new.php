9a454804ed50c63532aba225d4f2db20a615a839
mohe2015 <Moritz.Hedtke@t-online.de>
2019-05-17 21:30:10 +0200

    Implement creating teacher

---
+++ teacher/new.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../head.php';
+if (!isset($_SESSION['name'])) {
+  header("Location: /login.php");
+  die("not logged in");
+}
+if (!empty($_POST)) {
+  $teacher = new Teacher($_POST); // TODO check if this can modify the type
+  try {
+    $teacher->password = "none";
+    $teacher->save();
+  } catch (Exception $e) {
+    die($e->getMessage());
+  }
+  header("Location: /teachers");
+  die();
+}
+?>
+
+<h1>Lehrer erstellen</h1>
+<?php
+require_once 'form.php';
+?>
