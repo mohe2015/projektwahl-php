0fd5f3a41a471d1777e5925eddd24182929dc023
mohe2015 <Moritz.Hedtke@t-online.de>
2019-05-27 16:31:19 +0200

    New idea

--- election.php
+++ election.php
@@ -28,27 +28,12 @@ $projects = Projects::all();
             <td><a href="/project/view.php?<?php echo $project->id ?>"><?php echo htmlspecialchars($project->title) ?></a></td>
             <td>unbekannt</td>
             <td>
-              <div class="radio-group">
-                <input type="radio" id="option-one" name="selector"><label for="option-one">1.</label><input type="radio" id="option-two" name="selector"><label for="option-two">2.</label><input type="radio" id="option-three" name="selector"><label for="option-three">3.</label><input type="radio" id="option-four" name="selector"><label for="option-four">4.</label><input type="radio" id="option-five" name="selector"><label for="option-five">5.</label><input type="radio" id="option-sixth" name="selector"><label for="option-sixth">X</label>
-              </div>
-              <!--  <label class="">
-                  <input type="radio" name="projects[<?php echo $project->id ?>][choice]" value="1" autocomplete="off"> 1.
-                </label>
-                <label class="">
-                  <input type="radio" name="projects[<?php echo $project->id ?>][choice]" value="2" autocomplete="off"> 2.
-                </label>
-                <label class="">
-                  <input type="radio" name="projects[<?php echo $project->id ?>][choice]" value="3" autocomplete="off"> 3.
-                </label>
-                <label class="">
-                  <input type="radio" name="projects[<?php echo $project->id ?>][choice]" value="4" autocomplete="off"> 4.
-                </label>
-                <label class="">
-                  <input type="radio" name="projects[<?php echo $project->id ?>][choice]" value="5" autocomplete="off"> 5.
-                </label>
-                <label class="">
-                  <input type="radio" name="projects[<?php echo $project->id ?>][choice]" value="0" autocomplete="off"> X
-                </label>-->
+              <form method="post">
+                <input type="hidden" name="project_id" value="<?php echo $project->id ?>">
+                <input type="hidden" name="choice_id" value="1">
+                <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
+                <button type="submit">1.</button>
+              </form>
             </td>
           </tr>
         <?php endforeach;?>
