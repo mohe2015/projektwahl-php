1e7d46080f02e873994c830b68116cd44e0b26fa
mohe2015 <Moritz.Hedtke@t-online.de>
2019-05-28 19:48:22 +0200

    Fix if network fails

--- election.php
+++ election.php
@@ -29,14 +29,14 @@ $projects = Projects::all();
             <td><a href="/projects/view.php?<?php echo $project->id ?>"><?php echo htmlspecialchars($project->title) ?></a></td>
             <td>
               <?php
-              $rank = Choices::find($_SESSION['id'], $project->id)->rank;
+              $rank = Choices::find($_SESSION['id'], $project->id)->rank; // TODO natural join
               for ($i = 1; $i <= 5; $i++):
               ?>
               <form class="choice-form" method="post" style="display: inline;">
                 <input type="hidden" name="project_id" value="<?php echo $project->id ?>">
                 <input type="hidden" name="choice_id" value="<?php echo $i ?>">
                 <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
-                <button type="submit" <?php echo $rank == $i ? "disabled=disabled" : "" ?>><?php echo $i ?>.</button>
+                <button data-rank="<?php echo $i ?>" type="submit" <?php echo $rank == $i ? "disabled=disabled" : "" ?>><?php echo $i ?>.</button>
               </form>
               <?php
               endfor;
@@ -45,7 +45,7 @@ $projects = Projects::all();
                 <input type="hidden" name="project_id" value="<?php echo $project->id ?>">
                 <input type="hidden" name="choice_id" value="0">
                 <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
-                <button type="submit" <?php echo $rank == 0 ? "disabled=disabled" : "" ?>>X</button>
+                <button data-rank="0" type="submit" <?php echo $rank == 0 ? "disabled=disabled" : "" ?>>X</button>
               </form>
             </td>
           </tr>
@@ -57,14 +57,26 @@ $projects = Projects::all();
 function onChoiceSubmit(event) {
   event.preventDefault();
 
+  let oldRank = this.parentNode.querySelector('button[type="submit"]:disabled').getAttribute('data-rank');
+  let newRank = this.querySelector('button[type="submit"]').getAttribute('data-rank');
+
+  console.log(oldRank);
+  console.log(newRank);
+
   this.parentNode.querySelectorAll('button[type="submit"]').forEach(e => e.setAttribute('disabled', null));
 
   fetch("/election.php", {
     method: 'POST',
     body: new FormData(this)
   }).then((data) => {
-    console.log(data);
-    this.parentNode.querySelectorAll('button[type="submit"]').forEach(e => e.removeAttribute('disabled'));
+    [...this.parentNode.querySelectorAll('button[type="submit"]')]
+      .filter(x => x.getAttribute('data-rank') != newRank)
+      .forEach(e => e.removeAttribute('disabled'));
+  },
+  (error) => {
+    [...this.parentNode.querySelectorAll('button[type="submit"]')]
+      .filter(x => x.getAttribute('data-rank') != oldRank)
+      .forEach(e => e.removeAttribute('disabled'));
   });
 
   return false;
