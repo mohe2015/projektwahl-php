405044792154b96555eea3f81f29c22c0e01ad68
mohe2015 <Moritz.Hedtke@t-online.de>
2019-06-07 18:18:08 +0200

    Add base library

--- head.php
+++ head.php
@@ -24,6 +24,7 @@ function active_exact($path) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/index.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <script src="/base.js"></script>
     <title>Projektwahl</title>
   </head>
   <body>
