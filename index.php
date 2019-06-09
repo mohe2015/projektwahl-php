130381e03e740502113e1be8d27e2caf2cde1dda
mohe2015 <Moritz.Hedtke@t-online.de>
2019-06-09 19:29:37 +0200

    Working migration instructions

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ CREATE EXTENSION dblink;
 
 // TODO we also need the ids
 
-INSERT INTO projects (title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, requirements, random_assignments)
-SELECT title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed FROM dblink('dbname=projektwahl_production', 'SELECT title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed FROM projects') AS t1(title TEXT, info TEXT, place TEXT, costs DECIMAL, min_grade INT, max_grade INT, min_participants INT, max_participants INT, presentation_type TEXT, helper_count TEXT, random_assignment_allowed BOOLEAN);
+INSERT INTO projects (id, title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, requirements, random_assignments)
+SELECT id, title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed FROM dblink('dbname=projektwahl_production', 'SELECT id, title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed FROM projects') AS t1(id INT, title TEXT, info TEXT, place TEXT, costs DECIMAL, min_grade INT, max_grade INT, min_participants INT, max_participants INT, presentation_type TEXT, helper_count TEXT, random_assignment_allowed BOOLEAN);
 
-INSERT INTO users (name, password, type, project_leader, class, grade, away)
-SELECT name, password, 'student', project_leader, class, grade, away FROM dblink('dbname=projektwahl_production', 'SELECT
-name, password_digest, clazz, grade, is_away, project_leader_id FROM users') AS t1(
+INSERT INTO users (id, name, password, type, project_leader, class, grade, away)
+SELECT id, name, password, 'student', project_leader, class, grade, away FROM dblink('dbname=projektwahl_production', 'SELECT
+id, name, password_digest, clazz, grade, is_away, project_leader_id FROM users') AS t1(id INT,
 name TEXT, password TEXT, class TEXT, grade INT, away BOOL, project_leader INT);
 
 INSERT INTO choices (rank, project, student)
