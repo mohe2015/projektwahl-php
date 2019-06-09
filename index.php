8a99583801722112365cd57486c8f52e8dd50439
mohe2015 <Moritz.Hedtke@t-online.de>
2019-06-09 19:10:25 +0200

    Working on migration

--- index.php
+++ index.php
@@ -9,3 +9,8 @@ require_once __DIR__ . '/head.php';
 <p>Diese Software wurde von Moritz Hedtke entwickelt.</p>
 
 <p>Der Quellcode ist aus Transparenzgründen unter <a target="_blank" rel="noopener noreferrer" href="https://github.com/mohe2015/projektwahl-php">https://github.com/mohe2015/projektwahl-php</a> einzusehen.</p>
+
+CREATE EXTENSION dblink;
+
+INSERT INTO projects (title, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, requirements, random_assignments)
+SELECT titel, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed FROM dblink('dbname=projektwahl_production', 'SELECT titel, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed FROM projects') AS t1(titel, info, place, costs, min_grade, max_grade, min_participants, max_participants, presentation_type, helper_count, random_assignment_allowed);
