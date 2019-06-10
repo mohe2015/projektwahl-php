0ee7f7b75488f5a1e4ca1923141fefae453b5595
mohe2015 <Moritz.Hedtke@t-online.de>
2019-06-10 13:07:15 +0200

    Some testing changes

--- students/calculate.php
+++ students/calculate.php
@@ -1,4 +1,29 @@
 <?php
+function disable_ob() {
+    // Turn off output buffering
+    ini_set('output_buffering', 'off');
+    // Turn off PHP output compression
+    ini_set('zlib.output_compression', false);
+    // Implicitly flush the buffer(s)
+    ini_set('implicit_flush', true);
+    ob_implicit_flush(true);
+    // Clear, and turn off output buffering
+    while (ob_get_level() > 0) {
+        // Get the curent level
+        $level = ob_get_level();
+        // End the buffering
+        ob_end_clean();
+        // If the current level has not changed, abort
+        if (ob_get_level() == $level) break;
+    }
+    // Disable apache output buffering/compression
+    if (function_exists('apache_setenv')) {
+        apache_setenv('no-gzip', '1');
+        apache_setenv('dont-vary', '1');
+    }
+}
+disable_ob();
+
 header("Content-Type: text/plain");
 $allowed_users = array("admin");
 require_once __DIR__ . '/../header.php';
@@ -48,7 +73,8 @@ $stmt->execute();
 $choices = $stmt->fetchAll(PDO::FETCH_CLASS, 'Choice');
 
 // maximize rating points
-$out = fopen('/tmp/problem.lp', 'w'); // TODO temp file
+$problem_filename = tempnam("/tmp", "problem");
+$out = fopen($problem_filename, 'w');
 $stdout = fopen('php://output', 'w');
 fwrite($out, "Maximize\n");
 fwrite($out, " obj:");
@@ -60,6 +86,12 @@ foreach ($choices as $choice) {
 }
 foreach ($assoc_projects as $project_id => $project) {
   fwrite($out, " - 11000 P$project_id" . "_o");
+  fwrite($out, " - 11000 P$project_id" . "_u");
+}
+foreach ($assoc_students as $student_id => $student) {
+  fwrite($out, " - 11000 S$student_id" . "_f1");
+  fwrite($out, " - 11000 S$student_id" . "_f2");
+  fwrite($out, " - 11000 S$student_id" . "_f3");
 }
 fwrite($out, "\nSubject To");
 
@@ -126,6 +158,8 @@ foreach ($grouped_choices as $student_id => $choices) {
   if ($project_leader) {
     fwrite($out, " + P$project_leader" . "_e");
   }
+  fwrite($out, " + S$student_id" . "_f2");
+  fwrite($out, " - S$student_id" . "_f3");
   fwrite($out, " = 1");
 
   // student only in project if it exists
@@ -138,8 +172,8 @@ foreach ($grouped_choices as $student_id => $choices) {
     #   in project (1)     and project exists (0)
     # 2
     #   in project (1)     and project doesn't exist (1)
-    fwrite($out, "\n S$choice->student" . "_P$choice->project" . "_e1: " . choice2string($choice) . " + P$choice->project" . "_ne <= 1");
-    fwrite($out, "\n S$choice->student" . "_P$choice->project" . "_e2: " . choice2string($choice) . " + P$choice->project" . "_ne >= 0");
+    fwrite($out, "\n S$choice->student" . "_P$choice->project" . "_e1: " . choice2string($choice) . " + P$choice->project" . "_ne - S$student_id" . "_f1 <= 1");
+    fwrite($out, "\n S$choice->student" . "_P$choice->project" . "_e2: " . choice2string($choice) . " + P$choice->project" . "_ne - S$student_id" . "_f1 >= 0");
   }
 }
 
@@ -157,6 +191,7 @@ foreach ($assoc_projects as $project_id => $project) {
   foreach ($choices as $choice) {
     fwrite($out, " + " . choice2string($choice));
   }
+  fwrite($out, " + P$project_id" . "_u");
   fwrite($out, " + $project->min_participants P$choice->project" . "_ne >= $project->min_participants");
 
   fwrite($out, "\n P$project_id" . "_check_o: ");
@@ -173,15 +208,31 @@ fwrite($out, "\nBounds");
 
 foreach ($assoc_projects as $project_id => $project) {
   fwrite($out, "\n 0 <= P$project_id" . "_o");
+  fwrite($out, "\n 0 <= P$project_id" . "_u");
+}
+
+foreach ($assoc_students as $student_id => $student) {
+  fwrite($out, "\n 0 <= S$student_id" . "_f2");
+  fwrite($out, "\n 0 <= S$student_id" . "_f3");
 }
 
 fwrite($out, "\nGeneral\n");
 
 foreach ($assoc_projects as $project_id => $project) {
   fwrite($out, " P$project_id" . "_o");
+  fwrite($out, " P$project_id" . "_u");
+}
+
+foreach ($assoc_students as $student_id => $student) {
+  fwrite($out, " S$student_id" . "_f2");
+  fwrite($out, " S$student_id" . "_f3");
 }
 
 fwrite($out, "\nBinary\n");
+
+foreach ($assoc_students as $student_id => $student) {
+  fwrite($out, " S$student_id" . "_f1");
+}
 
 foreach ($assoc_projects as $project_id => $project) {
   $choices = $project_grouped_choices[$project_id];
@@ -195,10 +246,12 @@ fwrite($out, "\nEnd\n");
 fclose($out);
 
 // glpsol --check --lp /tmp/problem.lp --wmps /tmp/problem.mips
+// now lp_solve would also work
 
-passthru("glpsol --cbg --lp /tmp/problem.lp -o /tmp/solution.txt");
+$solution_filename = tempnam("/tmp", "solution");
+passthru("glpsol --lp $problem_filename -o $solution_filename --dual");
 
-$solution_file = fopen("/tmp/solution.txt", "r");
+$solution_file = fopen("$solution_filename", "r");
 
 $solution = array();
 while (!feof($solution_file))  {
@@ -220,11 +273,11 @@ foreach ($assoc_projects as $project_id => $project) {
   foreach ($choices as $choice) {
     if ($solution[choice2string($choice)] === 1) {
       $sum++;
-      print($assoc_students[$choice->student]->name . " in " . $project->title . "\n");
+      //print($assoc_students[$choice->student]->name . " in " . $project->title . "\n");
     }
   }
   if ($solution["P$project_id" . "_e"] === 1) {
-    print($project->title . " findet statt. ($sum / $project->max_participants)\n");
+    //print($project->title . " findet statt. ($sum / $project->max_participants)\n");
   }
   if ($solution["P$project_id" . "_ne"] === 1) {
     print($project->title . " findet NICHT statt.\n");
