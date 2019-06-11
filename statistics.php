55e907acb8c9cc733a15d12f02a916084826f08e
mohe2015 <Moritz.Hedtke@t-online.de>
2019-06-11 17:41:12 +0200

    Start switching to d3

--- statistics.php
+++ statistics.php
@@ -5,54 +5,96 @@ require_once __DIR__ . '/head.php';
 
 <h1>Statistik</h1>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.7.1/cytoscape.min.js" integrity="sha256-L/QqCCV+29/QBDhoDxOz7wTv5x0PNY90vNk9s3Kur+E=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.2/d3.js" integrity="sha256-kX9/pjvgpDgSmoSfzAlYJeICaZXca16iu+c3F6ueKRo=" crossorigin="anonymous"></script>
+<script src="http://marvl.infotech.monash.edu/webcola/cola.v3.js"></script>
 
-<style>
-#cy {
-  width: 100%;
-  height: 300px;
-  display: block;
-}
-</style>
-
-<div id="cy"></div>
+<div id="graph"></div>
 
 <script>
+// Copies a variable number of methods from source to target.
+d3.rebind = function(target, source) {
+  var i = 1, n = arguments.length, method;
+  while (++i < n) target[method = arguments[i]] = d3_rebind(target, source, source[method]);
+  return target;
+};
+
+// Method is assumed to be a standard D3 getter-setter:
+// If passed with no arguments, gets the value.
+// If passed with arguments, sets the value and returns the target.
+function d3_rebind(target, source, method) {
+  return function() {
+    var value = method.apply(source, arguments);
+    return value === source ? target : value;
+  };
+}
+
 fetch('/graph.php')
 .then(function(response) {
   return response.json();
 })
 .then(function(data) {
-    var cy = cytoscape({
 
-      container: document.getElementById('cy'), // container to render in
-
-      elements: data,
-
-      style: [ // the stylesheet for the graph
-        {
-          selector: 'node',
-          style: {
-            'background-color': '#666',
-            'label': 'data(id)'
-          }
-        },
-
-        {
-          selector: 'edge',
-          style: {
-            'width': 3,
-            'line-color': '#ccc',
-            'target-arrow-color': '#ccc',
-            'target-arrow-shape': 'triangle'
-          }
-        }
-      ],
-
-      layout: {
-        name: 'cose',
-        randomize: true
-      }
-    });
 });
+
+async function test() {
+  color = d3.scaleOrdinal(d3.schemeCategory10)
+  height = 500
+  width = 500
+  data = await d3.json("https://gist.githubusercontent.com/mbostock/4062045/raw/5916d145c8c048a6e3086915a6be464467391c62/miserables.json")
+
+  const nodes = data.nodes.map(d => Object.create(d));
+  const index = new Map(nodes.map(d => [d.id, d]));
+  const links = data.links.map(d => Object.assign(Object.create(d), {
+    source: index.get(d.source),
+    target: index.get(d.target)
+  }));
+
+  const svg = d3.select("#graph");
+
+  const layout = cola.d3adaptor(d3)
+      .size([width, height])
+      .nodes(nodes)
+      .links(links)
+      .jaccardLinkLengths(40, 0.7)
+      .start(30);
+
+  const link = svg.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .enter().append("line")
+      .attr("stroke-width", d => Math.sqrt(d.value));
+
+  const node = svg.append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+      .attr("r", 5)
+      .attr("fill", d => color(d.group))
+      .call(layout.drag);
+
+  node.append("title")
+      .text(d => d.id);
+
+  layout.on("tick", () => {
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+  });
+
+  invalidation.then(() => layout.stop());
+  //return svg.node();
+}
+
+test();
+
 </script>
