3af9363a01e3faba82d661babccdbe166192991c
mohe2015 <Moritz.Hedtke@t-online.de>
2019-06-12 12:27:39 +0200

    Small update

--- students/generate_passwords.php
+++ students/generate_passwords.php
@@ -8,37 +8,46 @@ $students = Students::all();
 $timers->endTimer('all_students');
 ?>
 
-<h1>Passwörter</h1>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+  <h1>Passwörter</h1>
 
-<div class="responsive">
-  <table>
-    <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Klasse</th>
-          <th scope="col">Passwort</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $timers->startTimer('generate_passwords');
-        $db->beginTransaction();
-        foreach ($students as $student) :?>
-          <?php
-            $password = bin2hex(random_bytes(5));
-            $student->password = password_hash($password, PASSWORD_DEFAULT, $options);
-            $student->save();
-          ?>
+  <div class="responsive">
+    <table>
+      <thead>
           <tr>
-            <td><?php echo htmlspecialchars($student->name) ?></td>
-            <td><?php echo htmlspecialchars($student->class) ?></td>
-            <td><?php echo htmlspecialchars($password) ?></td>
+            <th scope="col">Name</th>
+            <th scope="col">Klasse</th>
+            <th scope="col">Passwort</th>
           </tr>
-        <?php endforeach;
-        $db->commit();
-        $timers->endTimer('generate_passwords');
-        header('Server-Timing: ' . $timers->getTimers());
-        ?>
-      </tbody>
-  </table>
-</div>
+        </thead>
+        <tbody>
+          <?php
+          $timers->startTimer('generate_passwords');
+          $db->beginTransaction();
+          foreach ($students as $student) :?>
+            <?php
+              $password = bin2hex(random_bytes(5));
+              $student->password = password_hash($password, PASSWORD_DEFAULT, $options);
+              $student->save();
+            ?>
+            <tr>
+              <td><?php echo htmlspecialchars($student->name) ?></td>
+              <td><?php echo htmlspecialchars($student->class) ?></td>
+              <td><?php echo htmlspecialchars($password) ?></td>
+            </tr>
+          <?php endforeach;
+          $db->commit();
+          $timers->endTimer('generate_passwords');
+          header('Server-Timing: ' . $timers->getTimers());
+          ?>
+        </tbody>
+    </table>
+  </div>
+<?php else: ?>
+  <br />
+  <form method="post">
+    Möchtest Du wirklich die Passwörter generieren?
+    <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
+    <button type="submit">Ja</button>
+  </form>
+<?php endif; ?>
--- teachers/generate_passwords.php
+++ teachers/generate_passwords.php
@@ -8,35 +8,44 @@ $teachers = Teachers::all();
 $timers->endTimer('all_teachers');
 ?>
 
-<h1>Passwörter</h1>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+  <h1>Passwörter</h1>
 
-<div class="responsive">
-  <table>
-    <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Passwort</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $timers->startTimer('generate_passwords');
-        $db->beginTransaction();
-        foreach ($teachers as $teacher) :?>
-          <?php
-            $password = bin2hex(random_bytes(5));
-            $teacher->password = password_hash($password, PASSWORD_DEFAULT, $options);
-            $teacher->save();
-          ?>
+  <div class="responsive">
+    <table>
+      <thead>
           <tr>
-            <td><?php echo htmlspecialchars($teacher->name) ?></td>
-            <td><?php echo htmlspecialchars($password) ?></td>
+            <th scope="col">Name</th>
+            <th scope="col">Passwort</th>
           </tr>
-        <?php endforeach;
-        $db->commit();
-        $timers->endTimer('generate_passwords');
-        header('Server-Timing: ' . $timers->getTimers());
-        ?>
-      </tbody>
-  </table>
-</div>
+        </thead>
+        <tbody>
+          <?php
+          $timers->startTimer('generate_passwords');
+          $db->beginTransaction();
+          foreach ($teachers as $teacher) :?>
+            <?php
+              $password = bin2hex(random_bytes(5));
+              $teacher->password = password_hash($password, PASSWORD_DEFAULT, $options);
+              $teacher->save();
+            ?>
+            <tr>
+              <td><?php echo htmlspecialchars($teacher->name) ?></td>
+              <td><?php echo htmlspecialchars($password) ?></td>
+            </tr>
+          <?php endforeach;
+          $db->commit();
+          $timers->endTimer('generate_passwords');
+          header('Server-Timing: ' . $timers->getTimers());
+          ?>
+        </tbody>
+    </table>
+  </div>
+<?php else: ?>
+  <br />
+  <form method="post">
+    Möchtest Du wirklich die Passwörter generieren?
+    <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
+    <button type="submit">Ja</button>
+  </form>
+<?php endif; ?>
