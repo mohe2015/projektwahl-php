85b455c3f9536f1097d5f4da19750209f8db3a4f
mohe2015 <mohe2015@users.noreply.github.com>
2019-09-19 19:39:40 +0200

    Add license headers

--- students/student.php
+++ students/student.php
@@ -1,3 +1,22 @@
+<?php
+/*
+projektwahl-php - manage project selection for many people
+Copyright (C) 2019 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+
+projektwahl-php is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+projektwahl-php is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with projektwahl-php.  If not, see <https://www.gnu.org/licenses/>.
+*/
+?>
 <div style="page-break-inside: avoid;">
   <h2><?php echo htmlspecialchars($student->name) ?> <a class="print-display-none" href="<?php echo $ROOT ?>/students/edit.php?<?php echo $student->id ?>"><i class="fas fa-pen"></i></a> <a class="print-display-none" href="<?php echo $ROOT ?>/students/delete.php?<?php echo $student->id ?>"><i class="fas fa-trash"></i></a></h2>
   <b>Klasse: </b><?php echo htmlspecialchars($student->class) ?><br>
