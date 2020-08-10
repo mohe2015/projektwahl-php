a7d05cebcf4e71a2e7bcc81f2d44b10e4c581d16
mohe2015 <Moritz.Hedtke@t-online.de>
2020-08-10 14:07:01 +0200

    Remove some files

--- config.nix
+++ config.nix
@@ -7,16 +7,19 @@ services.httpd = {
     virtualHosts = {
         "localhost" = {
             documentRoot = "/home/moritz/Documents/projektwahl-php";
-            
+
             extraConfig = ''
-            DirectoryIndex index.html index.php
-            
-            RewriteEngine On
-            RewriteCond %{REQUEST_FILENAME} !-f
-            RewriteCond %{REQUEST_FILENAME} !-d
-            RewriteCond %{REQUEST_URI} !/subfolder
-            RewriteRule ^(.*)$ /index.html [L,QSA]
+                DirectoryIndex index.html index.php
+
+                RewriteEngine On
+                RewriteCond %{REQUEST_FILENAME} !-f
+                RewriteCond %{REQUEST_FILENAME} !-d
+                RewriteCond %{REQUEST_URI} !/css
+                RewriteCond %{REQUEST_URI} !/js
+                RewriteCond %{REQUEST_URI} !/fontawesome
+                RewriteCond %{REQUEST_URI} !/api
+                RewriteRule ^(.*)$ /index.html [L,QSA]
             '';
         };
     };
-  };
+};
